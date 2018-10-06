347b0582a77110fae99dfed5aca34bd921212196
Grace Choi <oiograceoio@gmail.com>
2018-10-06 04:04:30 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -23,5 +23,9 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-
+  if phone.scan(/[0-9]/) == nil
+    return false
+  else
+    return true
+  end
 end
